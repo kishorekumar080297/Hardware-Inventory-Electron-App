2b896196199eb755fade07ba95b70a2bc926863f
Kishore Kumar G <kishorekumar080297@gmail.com>
2017-03-20 11:17:46 +0530

    Added Home Page

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,34 @@ imprint.config(function($routeProvider, $locationProvider){
    $routeProvider.when('/main', {
      cache: false,
      templateUrl: 'views/main.html',
-     controller:'Main'
+     controller:'MainCtrl'
+   })
+   $routeProvider.when('/main.home', {
+     url:'/home',
+     views:{
+    'main-home':{
+       templateUrl: 'views/main-home.html',
+       controller:'HomeCtrl'
+    }
+    }
+   })
+   $routeProvider.when('/main.add', {
+     url:'/home',
+     views:{
+    'main-add':{
+       templateUrl: 'views/main-add.html',
+       controller:'AddCtrl'
+    }
+  }
+   })
+   $routeProvider.when('/main.view', {
+     url:'/view',
+     views:{
+    'main-home':{
+       templateUrl: 'views/main-view.html',
+       controller:'ViewCtrl'
+    }
+    }
    })
   //  .otherwise({
   //    redirectTo:'/main'
--- www/js/controller.js
+++ www/js/controller.js
@@ -2,8 +2,25 @@ angular.module('imprintControllers',[])
 .controller('Welcome',function($timeout,$location){
   $timeout(function() {
      $location.path('/main');
-     }, 3000);
+   }, 3000);
 })
-.controller('Main',function(){
+.controller('MainCtrl',function($scope,$location){
+  $scope.goToHome=function(){
+    $location.path('/main.home');
+  }
+  $scope.goToAdd=function(){
+    $location.path('/main.add');
+  }
+  $scope.goToView=function(){
+    $location.path('/main.view');
+  }
+})
+.controller('HomeCtrl',function(){
+
+})
+.controller('AddCtrl',function(){
+
+})
+.controller('ViewCtrl',function(){
 
 })
